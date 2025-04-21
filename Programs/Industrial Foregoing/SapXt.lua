27c2ac033a33dcbfa1f074bd9c47217fbd8e24ad
Megallix <megallix@gmail.com>
2025-04-21 06:25:22 -0700

    Update SapXt.lua
    bug fix

--- Programs/Industrial Foregoing/SapXt.lua
+++ Programs/Industrial Foregoing/SapXt.lua
@@ -2,16 +2,18 @@ TermLib = require("TermLib")
 
 --initialize
 TermLib.reset(colors.gray)
-TermLib.printc({"SapXt, V1.0.6"}, colors.pink, colors.gray)
+TermLib.printc({"SapXt, V1.0.7"}, colors.pink, colors.gray)
 print()
 TermLib.printc({"Managing","Sap Extractor"}, colors.yellow, colors.gray)
 print()
 
 --places a log if nothing below the turtle
 --acacia logs are ideal for sap
-while turtle.getItemCount() > 0  and not turtle.detectDown() do
-    turtle.placeDown(1)
-    TermLib.printx({"Placed Acacia Log..."}, colors.blue, colors.gray)
+while turtle.getItemCount() > 0 do
+    if not turtle.detectDown() then
+        turtle.placeDown(1)
+        TermLib.printx({"Placed Acacia Log..."}, colors.blue, colors.gray)
+    end
 end
 
 --reboots turtle when out of logs
