6f33816beb843de9fc9fb07fbf69067b581f8d1e
Megallix <megallix@gmail.com>
2025-04-22 03:32:57 -0700

    Update TurtleLib.lua

--- _Libraries/TurtleLib.lua
+++ _Libraries/TurtleLib.lua
@@ -1,5 +1,7 @@
---TurtleLib V1.0.0
+--TurtleLib V1.0.0.2
 
+--Cycle through inventory.
+--Returns either table of inventory, or search term if found.
 local function invcheck(search)
     local inventory = {}
     for i = 1, 16 do
@@ -8,4 +10,8 @@ local function invcheck(search)
     return inventory
 end
 
-print(invcheck())
+for i, v in ipairs(invcheck()) do --debugging
+    for n, a in ipairs(v) do
+        print(n, a)
+    end
+end
