910d3134ba813d9175671bbcf06c5fd53e49e47f
Megallix <megallix@gmail.com>
2025-04-22 03:47:37 -0700

    Update TurtleLib.lua

--- _Libraries/TurtleLib.lua
+++ _Libraries/TurtleLib.lua
@@ -1,14 +1,5 @@
---TurtleLib V1.0.0.3
+--TurtleLib V2.0.0
 
---Return table of inventory.
-local function invcheck(search)
-    local inventory = {}
-    for i = 1, 16 do
-        inventory[i] = turtle.getItemDetail(i)
-    end
-    return inventory
-end
-
-for i, v in ipairs(invcheck()) do --debugging
-    print(i.v)
+for i = 1, 16 do
+    turtle.getItemDetail(i)
 end
