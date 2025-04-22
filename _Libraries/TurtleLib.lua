f0cb809afd267d8d0b6af15e1c22bc6dc7fd2811
Megallix <megallix@gmail.com>
2025-04-22 04:19:56 -0700

    Update TurtleLib.lua

--- _Libraries/TurtleLib.lua
+++ _Libraries/TurtleLib.lua
@@ -1,12 +1,14 @@
---TurtleLib V2.1.1
+--TurtleLib V2.1.2
 
 local function invfind()
     local inventory = {}
     for i = 1, 16 do
         inventory[i] = turtle.getItemDetail(i)
     end
-    for index, value in ipairs(inventory) do
-        print(inventory[index])
+    for index, value in pairs(inventory) do
+        for i, v in ipairs(inventory[index]) do
+            print(i,v)
+        end
     end
 end
 
